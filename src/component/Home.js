73d107dc6c109bde726c414e9c8c65bbbbf47d1c
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2020-12-13 22:06:08 +0530

    header footer added

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,14 @@
-function Home(){
-    return <h1>Home</h1>
+import './../assets/css/style.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header';
+import Footer from './Footer';
+function Home() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
